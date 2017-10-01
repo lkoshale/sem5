7ddd5c0a6eac53f44613940a11c92a9c901cd648
lkoshale <lkoshale@gmail.com>
2017-10-01 14:47:16 +0530

    Lab3 files added

---
+++ Compiler/lab/lab3/P3/Main.java
@@ -0,0 +1,41 @@
+import java.util.Hashtable;
+
+import syntaxtree.*;
+import visitor.*;
+
+
+
+public class Main {
+   public static void main(String [] args) {
+      try {
+         Node root = new MiniJavaParser(System.in).Goal();
+         
+         GJVoidDepthFirst<Arg> createTable = new GJVoidDepthFirst<Arg>();
+         Arg a = new Arg();
+         root.accept(createTable,a);
+         Hashtable<String,SymbolTable>Table = new  Hashtable<String,SymbolTable>();
+         Table = createTable.getTable();
+         
+         root.accept(new GJNoArguDepthFirst<Object>()); 
+         
+         //call Exten Algo
+         
+         //TODO generate the file
+      //   GJDepthFirst return String
+         
+         GJDepthFirst<String,Pass> obj = new GJDepthFirst<>();
+         obj.setTable();
+         obj.setSTable(Table);
+         root.accept(obj,null);
+         
+       //  System.out.println("Program parsed successfully "+GJNoArguDepthFirst.Table.size());
+         
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+} 
+
+
+
---
+++ Compiler/lab/lab3/P3/visitor/Data.java
@@ -0,0 +1,51 @@
+package visitor;
+
+import java.util.*;
+
+
+public class Data {	
+	
+	public static int Moffset = 0;
+	//key = cName+_+Mname
+	public HashMap<String,Method>FnAr = new HashMap<>();
+	
+	//iterate till first find
+	public List<String>VarMap = new ArrayList<>();
+
+	private String Extend = null;
+	public String getEx() {return this.Extend;}
+	public void setEx(String ex) {this.Extend = ex;}
+	
+	public void incMoff(int k) {
+		Moffset += k;
+	}
+	
+	public int getSize() {
+		return (this.VarMap.size()+1)*4;
+	}
+	
+	
+}
+
+class Method{
+	
+	public String name;
+	public String cName;
+	
+	public int offset;
+
+	
+	public List<String>Arg = new ArrayList<>();
+	
+	public Method(String name,String cName,int offset) {
+		this.cName = cName;
+		this.name = name;
+		this.offset = offset;
+	}
+	
+	//store var--> temp
+	public HashMap<String,String>Mvar = new HashMap<>();
+	
+//	public List<String>Arg
+	
+}
