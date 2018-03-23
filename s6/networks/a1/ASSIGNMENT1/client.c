7fc13922f2b14401ce16d401b96c928879e0e3a4
lkoshale <lkoshale@gmail.com>
2018-03-23 21:49:57 +0530

    network

--- s6/networks/a1/ASSIGNMENT1/client.c
+++ s6/networks/a1/ASSIGNMENT1/client.c
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[]) {
 
   int sockfd = socket(PF_INET,SOCK_STREAM,0);
   struct sockaddr_in address;
-  address.sin_family = AF_INET;
+  address.sin_family = AF_INET;+
   address.sin_port = htons(PORT);
   char ipServer[] = "127.0.0.1";
 
